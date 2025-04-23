1f89037322ace26e992cc4ed656cbaffb00bd0ea
tutorial <yilmaz.oezaydin@students.fhnw.ch>
2025-04-23 23:23:20 +0200

    fix some problem in station class

--- src/main/java/com/pym/scooter/model/Station.java
+++ src/main/java/com/pym/scooter/model/Station.java
@@ -1,6 +1,8 @@
 package com.pym.scooter.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+
 import java.util.List;
 
 @Entity
@@ -13,9 +15,11 @@ public class Station {
     private String name;
     private String location;
 
-    @OneToMany(mappedBy = "station")
+    @OneToMany(mappedBy = "station", cascade = CascadeType.ALL)
+    @JsonIgnore // prevent infinite recursion when serializing
     private List<Scooter> scooters;
 
+    // Constructors
     public Station() {}
 
     public Station(String name, String location) {
@@ -24,9 +28,12 @@ public class Station {
     }
 
     // Getters and Setters
-
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id; // helpful when creating new instances
     }
 
     public String getName() {
@@ -51,5 +58,15 @@ public class Station {
 
     public void setScooters(List<Scooter> scooters) {
         this.scooters = scooters;
+    }
+
+    // Optional: toString (good for debugging)
+    @Override
+    public String toString() {
+        return "Station{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                '}';
     }
 }
