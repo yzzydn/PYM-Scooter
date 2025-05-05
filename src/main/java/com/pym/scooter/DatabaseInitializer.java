b5c89054d0490b558ae8a793966744ca09c56eab
tutorial <yilmaz.oezaydin@students.fhnw.ch>
2025-05-05 13:32:30 +0200

    data

--- src/main/java/com/pym/scooter/DatabaseInitializer.java
+++ src/main/java/com/pym/scooter/DatabaseInitializer.java
@@ -1,10 +1,10 @@
 package com.pym.scooter;
 
+import com.pym.scooter.model.Station;
 import com.pym.scooter.model.Scooter;
 import com.pym.scooter.model.ScooterType;
-import com.pym.scooter.model.Station;
-import com.pym.scooter.repository.ScooterRepository;
 import com.pym.scooter.repository.StationRepository;
+import com.pym.scooter.repository.ScooterRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -23,20 +23,21 @@ public class DatabaseInitializer implements CommandLineRunner {
     public void run(String... args) {
         System.out.println("🚀 Loading test data...");
 
+        // Create stations
         Station s1 = new Station("Claraplatz");
         Station s2 = new Station("Bankveria");
-        Station s3 = new Station("SBB");
-        Station s4 = new Station("Badischen Bahnhof");
+        Station s3 = new Station("Sbb");
+        Station s4 = new Station("Badishen Bahnhof");
 
         stationRepository.save(s1);
         stationRepository.save(s2);
         stationRepository.save(s3);
         stationRepository.save(s4);
 
-        Scooter sc1 = new Scooter(ScooterType.LONG_DISTANCE, true, s1);
-        Scooter sc2 = new Scooter(ScooterType.SHORT_DISTANCE, true, s2);
-
-        scooterRepository.save(sc1);
-        scooterRepository.save(sc2);
+        // Create scooters
+        scooterRepository.save(new Scooter(ScooterType.SHORT_DISTANCE, true, s1));
+        scooterRepository.save(new Scooter(ScooterType.SHORT_DISTANCE, true, s2));
+        scooterRepository.save(new Scooter(ScooterType.LONG_DISTANCE, true, s3));
+        scooterRepository.save(new Scooter(ScooterType.LONG_DISTANCE, true, s4));
     }
 }
