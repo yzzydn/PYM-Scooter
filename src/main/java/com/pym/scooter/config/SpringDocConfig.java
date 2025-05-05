1092007c032032018ab1604037b88f9fd00c1295
tutorial <yilmaz.oezaydin@students.fhnw.ch>
2025-05-05 22:42:35 +0200

    new class

---
+++ src/main/java/com/pym/scooter/config/SpringDocConfig.java
@@ -0,0 +1,13 @@
+package com.pym.scooter.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI();
+    }
+}
